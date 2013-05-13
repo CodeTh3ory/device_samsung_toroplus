87608ebaf0dd7dc97975885b25e4c3ad024be9f8
CodeTh3ory <codeth3ory@gmail.com>
2013-05-13 08:44:45 -0400

    Derp

--- full_toroplus.mk
+++ full_toroplus.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/toroplus/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := axi0m_toroplus
+PRODUCT_NAME := full_toroplus
 PRODUCT_DEVICE := toroplus
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Full AOSP on Toroplus
